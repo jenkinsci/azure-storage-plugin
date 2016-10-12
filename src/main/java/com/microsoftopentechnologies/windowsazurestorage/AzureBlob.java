7e1e63e88164c1057825af809efa2afd797fbfa4
Samantha Schwarz <roguishmountain@gmail.com>
2016-10-12 15:36:10 -0400

    Remove hard coding of container name
    Refactor, add functionality, and bug fixes
    
    Add individual blobs and zipped blobs to API
     - Exposes the blob URL, MD5, and file size
    
    Add MD5 and size of file to AzureBlob class
    
    Add support for concurrent builds
     - This allows builds to run the upload to azure step without waiting for the previous build to complete
    
    Bug fixes
     - Download step didn't recognize zipped artifacts
     - Downloaded whole container under certain conditions
    
    Refactoring
    
    Remove unneeded manageArtifacts flag
    
    Fix excessive if statements

--- src/main/java/com/microsoftopentechnologies/windowsazurestorage/AzureBlob.java
+++ src/main/java/com/microsoftopentechnologies/windowsazurestorage/AzureBlob.java
@@ -1,25 +1,44 @@
 package com.microsoftopentechnologies.windowsazurestorage;
 
+import hudson.model.Api;
 import java.io.Serializable;
+import org.kohsuke.stapler.export.Exported;
+import org.kohsuke.stapler.export.ExportedBean;
 
+@ExportedBean
 public class AzureBlob implements Serializable {
 
 	private static final long serialVersionUID = -1873484056669542679L;
 	
 	private final String blobName;
 	private final String blobURL;
+	private final String md5;
+	private final long byteSize;
 	
-	public AzureBlob(String blobName, String blobURL) {
+	public AzureBlob(String blobName, String blobURL, String md5, long byteSize) {
 		this.blobName = blobName;
 		this.blobURL = blobURL;
+		this.md5 = md5;
+		this.byteSize = byteSize;
 	}
 
 	public String getBlobName() {
 		return blobName;
 	}
-	
+
+	@Exported
 	public String getBlobURL() {
 		return blobURL;
+	}
+
+	@Exported
+	public String getMd5() {
+		return md5;
+	}
+
+	@Exported
+	public long getSizeInBytes() {
+		return byteSize;
 	}
 
 	@Override
@@ -27,6 +46,4 @@ public class AzureBlob implements Serializable {
 		return "AzureBlob [blobName=" + blobName + ", blobURL="
 				+ blobURL + "]";
 	}
-	
-	
 }
