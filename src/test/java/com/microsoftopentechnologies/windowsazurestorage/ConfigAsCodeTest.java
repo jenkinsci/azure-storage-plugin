82687ac7a85744bfead9fa80e94bccaffc63c856
Tim Jacomb <t.jacomb@kainos.com>
2019-04-12 07:13:50 +0100

    Add configuration as code support (#147)
    * Bump jenkins core baseline
    
    * Fix tests
    
    * Configuration as code support
    
    * Rerun CI
    
    * Remove unused var
    
    * Use released version of JCasC
    
    * Restore test
    
    * Try bump jcasc version
    
    * Small tweaks, trying to fix windows build
    
    * Downgrade to compatible core version

---
+++ src/test/java/com/microsoftopentechnologies/windowsazurestorage/ConfigAsCodeTest.java
@@ -0,0 +1,70 @@
+package com.microsoftopentechnologies.windowsazurestorage;
+
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+import com.microsoftopentechnologies.windowsazurestorage.helper.AzureCredentials;
+import hudson.ExtensionList;
+import io.jenkins.plugins.casc.ConfigurationContext;
+import io.jenkins.plugins.casc.ConfiguratorRegistry;
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import io.jenkins.plugins.casc.model.CNode;
+import io.jenkins.plugins.casc.model.Mapping;
+import io.jenkins.plugins.casc.support.credentials.CredentialsRootConfigurator;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ConfigAsCodeTest {
+
+    @Rule
+    public JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
+
+    @Test
+    @ConfiguredWithCode("configuration-as-code.yml")
+    public void should_support_configuration_as_code() {
+        AzureCredentials credentials = (AzureCredentials) SystemCredentialsProvider.getInstance().getCredentials()
+                .get(0);
+
+        assertEquals(credentials.getScope(), CredentialsScope.GLOBAL);
+        assertEquals(credentials.getDescription(), "Account");
+        assertEquals(credentials.getStorageAccountName(), "a-storage-account");
+        assertEquals(credentials.getBlobEndpointURL(), "https://blob.core.windows.net/");
+        assertEquals(credentials.getId(), "storage-account");
+        assertNotNull(credentials.getStorageKey());
+    }
+
+    @Test
+    @ConfiguredWithCode("configuration-as-code.yml")
+    public void export_configuration() throws Exception {
+        ConfiguratorRegistry registry = ConfiguratorRegistry.get();
+        ConfigurationContext context = new ConfigurationContext(registry);
+
+        CredentialsRootConfigurator root = ExtensionList
+                .lookupSingleton(CredentialsRootConfigurator.class);
+        final CNode node = root.describe(root.getTargetComponent(context), context);
+
+        assertNotNull(node);
+        Mapping mapping = node.asMapping().get("system")
+                .asMapping()
+                .get("domainCredentials")
+                .asSequence()
+                .get(0)
+                .asMapping()
+                .get("credentials")
+                .asSequence()
+                .get(0)
+                .asMapping()
+                .get("azureStorageAccount")
+                .asMapping();
+
+        assertEquals(mapping.getScalarValue("scope"), "GLOBAL");
+        assertEquals(mapping.getScalarValue("description"), "Account");
+        assertEquals(mapping.getScalarValue("storageAccountName"), "a-storage-account");
+        assertEquals(mapping.getScalarValue("blobEndpointURL"), "https://blob.core.windows.net/");
+        assertEquals(mapping.getScalarValue("id"), "storage-account");
+        assertNotNull(mapping.getScalarValue("storageKey"));
+    }
+}
