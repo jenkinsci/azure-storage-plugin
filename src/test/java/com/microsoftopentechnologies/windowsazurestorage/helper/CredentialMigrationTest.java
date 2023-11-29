c70c43d58890ab5d37fabb384878374bc1c61071
Minreaux <9372700+Minreaux@users.noreply.github.com>
2023-11-29 14:06:01 -0800

    Fix missed changes in CredentialMigrationTest

--- src/test/java/com/microsoftopentechnologies/windowsazurestorage/helper/CredentialMigrationTest.java
+++ src/test/java/com/microsoftopentechnologies/windowsazurestorage/helper/CredentialMigrationTest.java
@@ -10,7 +10,6 @@ import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.CredentialsStore;
 import com.cloudbees.plugins.credentials.domains.Domain;
 import com.cloudbees.plugins.credentials.domains.DomainRequirement;
-
 import com.microsoftopentechnologies.windowsazurestorage.beans.StorageAccountInfo;
 import hudson.security.ACL;
 import jenkins.model.Jenkins;
@@ -28,6 +27,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+
 
 /**
  *
@@ -52,48 +52,46 @@ public class CredentialMigrationTest {
     "</com.microsoftopentechnologies.windowsazurestorage.beans.StorageAccountInfo>\n" +
     "</storageAccounts>\n" +
     "</com.microsoftopentechnologies.windowsazurestorage.AzureStoragePublisher_-WAStorageDescriptor>";
-
+    
     private static final String wrongConfigContent = "<?xml version='1.0' encoding='UTF-8'?>\n" +
     "<com.microsoftopentechnologies.windowsazurestorage.AzureStoragePublisher_-WAStorageDescriptor plugin='windows-azure-storage@0.3.3-SNAPSHOT'>\n" +
     "<storageAccounts>\n" +
     "<storageAccName>abcdef</storageAccName>\n" +
     "<storageAccountKey>12345</storageAccountKey>\n" +
     "<blobEndPointURL>https://blob.core.windows.net/</blobEndPointURL>\n" +
-    "<blobEndPointURL>https://cdn-resource-name.azureedge.net/</blobEndPointURL>\n" +
     "</storageAccounts>\n" +
     "</com.microsoftopentechnologies.windowsazurestorage.AzureStoragePublisher_-WAStorageDescriptor>";
-
+    
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
-    @ClassRule
+    @ClassRule 
     public static JenkinsRule j = new JenkinsRule();
 
     @Before
     public void setUp() {
         jenkinsInstance = Jenkins.getInstance();
+        
     }
-
     /**
      * Test of upgradeStorageConfig method, of class CredentialMigration.
      */
     @Test
     public void testUpgradeStorageConfig() throws Exception {
         System.out.println("upgradeStorageConfig");
-
+        
         String storageAccount = "abcdef";
         String storageAccountKey = "12345";
         String storageBlobURL = "http://test1/";
-        String storageCdnURL = "http://cdn-test1/";
         File configFile = testFolder.newFile(Constants.LEGACY_STORAGE_CONFIG_FILE);
         FileUtils.writeStringToFile(configFile, correctConfigContent);
         FileUtils.copyFileToDirectory(configFile, jenkinsInstance.root);
 
         CredentialMigration.upgradeStorageConfig();
-
+        
         CredentialsStore s = CredentialsProvider.lookupStores(jenkinsInstance).iterator().next();
 
         assertEquals(2, s.getCredentials(Domain.global()).size());
-
+                
         AzureStorageAccount.StorageAccountCredential u = new AzureStorageAccount.StorageAccountCredential(storageAccount, storageAccountKey, storageBlobURL, "");
         AzureStorageAccount storageCred = CredentialsMatchers.firstOrNull(CredentialsProvider.lookupCredentials(AzureStorageAccount.class, jenkinsInstance, ACL.SYSTEM, Collections.<DomainRequirement>emptyList()),
                 CredentialsMatchers.withId(u.getId()));
@@ -101,7 +99,7 @@ public class CredentialMigrationTest {
         assertEquals(u.getStorageAccountName(), storageCred.getStorageAccountName());
         assertEquals(u.getBlobEndpointURL(), storageCred.getBlobEndpointURL());
         assertEquals(u.getSecureKey().getPlainText(), storageCred.getPlainStorageKey());
-
+        
     }
 
     /**
@@ -114,27 +112,28 @@ public class CredentialMigrationTest {
         List<StorageAccountInfo> expResult = new ArrayList<StorageAccountInfo>();
         expResult.add(new StorageAccountInfo("abcdef", "12345", "http://test1/", ""));
         expResult.add(new StorageAccountInfo("12345", "abcdef", "http://test2/", ""));
-
+        
         File configFile = testFolder.newFile("test.xml");
         FileUtils.writeStringToFile(configFile, correctConfigContent);
         List<StorageAccountInfo> result = CredentialMigration.getOldStorageConfig(configFile);
-
+        
         assertEquals(expResult.size(), result.size());
         assertEquals(expResult.get(0).getStorageAccName(), result.get(0).getStorageAccName());
         assertEquals(expResult.get(0).getBlobEndPointURL(), result.get(0).getBlobEndPointURL());
         assertEquals(expResult.get(0).getStorageAccountKey(), result.get(0).getStorageAccountKey());
-
+        
         assertEquals(expResult.get(1).getStorageAccName(), result.get(1).getStorageAccName());
         assertEquals(expResult.get(1).getBlobEndPointURL(), result.get(1).getBlobEndPointURL());
         assertEquals(expResult.get(1).getStorageAccountKey(), result.get(1).getStorageAccountKey());
-
+        
         expResult.clear();
         result.clear();
-
+        
         configFile = testFolder.newFile("wrong.xml");
         FileUtils.writeStringToFile(configFile, wrongConfigContent);
         result = CredentialMigration.getOldStorageConfig(configFile);
-
+        
         assertEquals(0, result.size());
     }
+
 }
