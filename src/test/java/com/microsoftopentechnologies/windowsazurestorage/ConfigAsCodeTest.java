07317adf3031faa090eeedd0aed2b3854c5702b2
Minreaux <9372700+Minreaux@users.noreply.github.com>
2023-11-29 09:10:52 -0800

    Split ConfigAsCodeTest into no-CDN and CDN-enabled tests

--- src/test/java/com/microsoftopentechnologies/windowsazurestorage/ConfigAsCodeTest.java
+++ src/test/java/com/microsoftopentechnologies/windowsazurestorage/ConfigAsCodeTest.java
@@ -33,8 +33,22 @@ public class ConfigAsCodeTest {
         assertThat(credentials.getDescription(), is("Account"));
         assertThat(credentials.getStorageAccountName(), is("a-storage-account"));
         assertThat(credentials.getBlobEndpointURL(), is("https://a-storage-account.blob.core.windows.net/"));
-        assertThat(credentials.getCdnEndpointURL(), is("https://cdn-resource-name.azureedge.net/"));
         assertThat(credentials.getId(), is("storage-account"));
+        assertNotNull(credentials.getStorageKey());
+    }
+
+    @Test
+    @ConfiguredWithCode("com/microsoftopentechnologies/windowsazurestorage/configuration-as-code.yml")
+    public void should_support_configuration_as_code_with_cdn() {
+        AzureStorageAccount credentials = (AzureStorageAccount) SystemCredentialsProvider.getInstance().getCredentials()
+                .get(1);
+
+        assertThat(credentials.getScope(), is(CredentialsScope.GLOBAL));
+        assertThat(credentials.getDescription(), is("CDN Enabled Account"));
+        assertThat(credentials.getStorageAccountName(), is("b-storage-account"));
+        assertThat(credentials.getBlobEndpointURL(), is("https://b-storage-account.blob.core.windows.net/"));
+        assertThat(credentials.getCdnEndpointURL(), is("https://cdn-resource-name.azureedge.net/"));
+        assertThat(credentials.getId(), is("cdn-enabled-storage-account"));
         assertNotNull(credentials.getStorageKey());
     }
 
@@ -66,8 +80,40 @@ public class ConfigAsCodeTest {
         assertThat(mapping.getScalarValue("description"), is("Account"));
         assertThat(mapping.getScalarValue("storageAccountName"), is("a-storage-account"));
         assertThat(mapping.getScalarValue("blobEndpointURL"), is("https://a-storage-account.blob.core.windows.net/"));
-        assertThat(mapping.getScalarValue("cdnEndpointURL"), is("https://cdn-resource-name.azureedge.net/"));
         assertThat(mapping.getScalarValue("id"), is("storage-account"));
+        assertNotNull(mapping.getScalarValue("storageKey"));
+    }
+
+    @Test
+    @ConfiguredWithCode("com/microsoftopentechnologies/windowsazurestorage/configuration-as-code.yml")
+    public void export_configuration_with_cdn() throws Exception {
+        ConfiguratorRegistry registry = ConfiguratorRegistry.get();
+        ConfigurationContext context = new ConfigurationContext(registry);
+
+        CredentialsRootConfigurator root = ExtensionList
+                .lookupSingleton(CredentialsRootConfigurator.class);
+        final CNode node = root.describe(root.getTargetComponent(context), context);
+
+        assertNotNull(node);
+        Mapping mapping = node.asMapping().get("system")
+                .asMapping()
+                .get("domainCredentials")
+                .asSequence()
+                .get(0)
+                .asMapping()
+                .get("credentials")
+                .asSequence()
+                .get(1)
+                .asMapping()
+                .get("azureStorageAccount")
+                .asMapping();
+
+        assertThat(mapping.getScalarValue("scope"), is("GLOBAL"));
+        assertThat(mapping.getScalarValue("description"), is("CDN Enabled Account"));
+        assertThat(mapping.getScalarValue("storageAccountName"), is("b-storage-account"));
+        assertThat(mapping.getScalarValue("blobEndpointURL"), is("https://b-storage-account.blob.core.windows.net/"));
+        assertThat(mapping.getScalarValue("cdnEndpointURL"), is("https://cdn-resource-name.azureedge.net/"));
+        assertThat(mapping.getScalarValue("id"), is("cdn-enabled-storage-account"));
         assertNotNull(mapping.getScalarValue("storageKey"));
     }
 }
